fdac505101164aa9afed977086ea2bd3e3d56f87
Wu Xutao <wublue1234@gmail.com>
2016-03-30 08:42:15 +0800

    add console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,28 @@
-console.log('This would be the main JS file.');
+var consoleMsg = {};
+
+consoleMsg.show = function() {
+	console.groupCollapsed("这都被你发现了");
+	  console.groupCollapsed("不要点了");
+	    console.groupCollapsed("里面什么也没有");
+	      console.groupCollapsed("不信你继续点");
+	        console.groupCollapsed("%cnull", "color: gray;");
+	          console.groupCollapsed("%cnull", "color: gray;");
+	            console.groupCollapsed("%cundefined", "color: gray;");
+	              console.groupCollapsed("%cundefined", "color: gray;");
+	                console.groupCollapsed("好吧");
+	                  console.groupCollapsed("这是我博客的日记");
+	                    console.log("http://4nix.github.com/blog/categories/diary.html");
+	                  console.groupEnd();
+	                console.groupEnd();
+	              console.groupEnd();
+	            console.groupEnd();
+	          console.groupEnd();
+	        console.groupEnd();
+	      console.groupEnd();
+	    console.groupEnd();
+	  console.groupEnd();
+	console.groupEnd();
+}
+
+
+consoleMsg.show();
